d13ee5d98ed7d0fb3357489523191f07cc130a8a
raphaelmachado <raphaelmachado@192.168.0.108>
2014-11-11 22:38:58 -0200

    ProjetoInterRevisteiro - Classe Upload criada

---
+++ src/br/edu/fatecriopreto/ProjetoRevisteiro/model/Upload.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.fatecriopreto.ProjetoRevisteiro.model;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author raphaelmachado
+ */
+public class Upload {
+
+    private String nomearquivo;
+    private String urlarquivo;
+
+    public Upload() {
+
+        nomearquivo = "";
+        urlarquivo = "";
+
+    }
+
+    public Upload(String nomearquivo, String urlarquivo) {
+
+        this.nomearquivo = nomearquivo;
+        this.urlarquivo = urlarquivo;
+
+    }
+
+    public void lerDados() {
+
+        nomearquivo = JOptionPane.showInputDialog("Digite o nome do arquivo: ");
+        urlarquivo = JOptionPane.showInputDialog("Digite a url do arquivo: ");
+
+    }
+}
